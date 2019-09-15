3f79912689bfc12f78db85579cbe9874f26d6816
Mohamed Mounir <m.mounir.f@gmail.com>
2019-09-15 21:27:38 +0200

    Updates

---
+++ precache-manifest.784df1168834415b6960a346e30f44bc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95263d39739e9c0d4de2c7366b7d37cf",
+    "url": "/mmounirf/event-registration-dashboard/index.html"
+  },
+  {
+    "revision": "c4c61a1ce77029461afe",
+    "url": "/mmounirf/event-registration-dashboard/static/css/2.84a5efba.chunk.css"
+  },
+  {
+    "revision": "1211a7c9468a54091570",
+    "url": "/mmounirf/event-registration-dashboard/static/css/main.3bed2a4b.chunk.css"
+  },
+  {
+    "revision": "c4c61a1ce77029461afe",
+    "url": "/mmounirf/event-registration-dashboard/static/js/2.f43efbde.chunk.js"
+  },
+  {
+    "revision": "1211a7c9468a54091570",
+    "url": "/mmounirf/event-registration-dashboard/static/js/main.50d0455b.chunk.js"
+  },
+  {
+    "revision": "8ce779e51d3750ae269c",
+    "url": "/mmounirf/event-registration-dashboard/static/js/runtime~main.f34caac6.js"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-300.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-300.d26871e8.woff2"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-300.fc84e998.ttf"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-400.35b07eb2.woff"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-400.3e1af3ef.ttf"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-400.73f0a88b.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-500.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-500.90d16760.woff2"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-500.d0884059.ttf"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-700.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-700.b52fac2b.woff2"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/mmounirf/event-registration-dashboard/static/media/Roboto-700.ee7b96fa.ttf"
+  },
+  {
+    "revision": "de33805c0651f8572e289641629f3745",
+    "url": "/mmounirf/event-registration-dashboard/static/media/dxiconsmaterial.de33805c.ttf"
+  },
+  {
+    "revision": "e606e2705d866e4ad02ebeb41aada886",
+    "url": "/mmounirf/event-registration-dashboard/static/media/dxiconsmaterial.e606e270.woff2"
+  },
+  {
+    "revision": "ef3cacd9a0a9db23d49a1d889c3586bc",
+    "url": "/mmounirf/event-registration-dashboard/static/media/dxiconsmaterial.ef3cacd9.woff"
+  }
+]);
